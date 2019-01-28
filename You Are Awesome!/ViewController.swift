59e9bd08c954946c99bf0fabe6935fa2e8a6c3f5
Victoria Wong <VictoriaWong@victorias-macbook-air.local>
2019-01-28 16:44:54 -0500

    2.20 Completed

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var awesomeImageView: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var soundSwitch: UISwitch!
     var awesomePlayer = AVAudioPlayer()
     var index = -1
     var imageIndex = -1
@@ -50,8 +51,13 @@ class ViewController: UIViewController {
         return newIndex
     }
 
+    @IBAction func soundSwitchPressed(_ sender: UISwitch) {
+        if soundSwitch.isOn == false && soundIndex != -1 {
+                awesomePlayer.stop()
+            }
+        }
+    
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        
         let messages = [ "You are Awesome!",
                          "You Are Great!",
                          "You Are Fantastic!",
@@ -71,13 +77,17 @@ class ViewController: UIViewController {
         imageIndex = nonRepeatingRandom(lastNumber: imageIndex, maxValue: numberOfImages)
         awesomeImageView.image = UIImage(named: "image\(imageIndex)")
         
+        //if soundSwitch.isOn == true {
+        if soundSwitch.isOn {
+        
         //Get a random number to use in our soundName file
         soundIndex = nonRepeatingRandom(lastNumber: soundIndex, maxValue: numberOfSounds)
         
         // Play a sound
         let soundName = "sound\(soundIndex)"
-        playSound(soundName: soundName, audioPlayer: &awesomePlayer)
         
+        playSound(soundName: soundName, audioPlayer: &awesomePlayer)
+        }
     }
 }
     
